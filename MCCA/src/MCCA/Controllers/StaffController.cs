3b4c0c41129b7b4249bfb9e3a8dd9997c664c930
Michael Johnson <mrjohnson9303@yahoo.com>
2016-06-27 21:49:23 -0700

    Created the necessary files for Directors and Staff such as layouts, controller and cshtml files to interact the website through GUIs. I also altered the title for the Home Index because it said SacMesa.

---
+++ MCCA/src/MCCA/Controllers/StaffController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MCCA.Controllers
+{
+    public class StaffController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult ManageSite()
+        {
+            return View();
+        }
+        public IActionResult ManagePersonalAccount()
+        {
+            return View();
+        }
+    }
+}
